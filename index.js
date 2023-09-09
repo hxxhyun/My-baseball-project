a4245cad40e80242c4345b6d64bd4cff5a87d49d
shin.heehyun <lupinworld22@gmail.com>
2023-09-09 14:36:06 +0900

    완성본

--- index.js
+++ index.js
@@ -11,33 +11,48 @@ for (var i = 0; i < 3; i++) {
     }
 }
 
-// 숫자 입력하기
-var input = prompt('숫자를 입력해주세요.');
-
-let inputNum = [];
-for (var i = 0; i < 3; i++) {
-inputNum.push(input[i]);
-}
-
-// 숫자 비교하기
-let try_cnt = 1;
 let ball_cnt = 0;
 let strike_cnt = 0;
-for (var i = 0; i < 3; i++) {
-    for (var j = 0; j < 3; j++) {
-        if (createNum[i] == inputNum[j]) {
-            if (i == j) {
-                strike_cnt++;
-            } else {
-                ball_cnt++;
+
+// 숫자 입력하기
+let try_cnt = 1;
+while (strike_cnt < 3) {
+    ball_cnt = 0;
+    strike_cnt = 0;
+    var input = prompt('숫자를 입력해주세요.');
+    
+    let inputNum = [];
+    for (var i = 0; i < 3; i++) {
+        inputNum.push(input[i]);
+    }
+
+    // 숫자 판단하기
+
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            if (createNum[i] == inputNum[j]) {
+                if (i == j) {
+                    strike_cnt++;
+                } else {
+                    ball_cnt++;
+                }
             }
         }
     }
+
+// 출력
+    if (strike_cnt == 3) {
+        console.log(`${try_cnt}번째 시도 : ${input}`);
+        console.log(`${strike_cnt}S`);
+        console.log(`${try_cnt}번만의 맞히셨습니다.`);
+        console.log('게임을 종료합니다.');
+    } else if (ball_cnt == 3) {
+        console.log(`${try_cnt}번째 시도 : ${input}`);
+        console.log(`${ball_cnt}B`);
+        try_cnt++;
+    } else {
+        console.log(`${try_cnt}번째 시도 : ${input}`);
+        console.log(`${ball_cnt}B${strike_cnt}S`);
+        try_cnt++;
+    }
 }
-
-console.log(`${try_cnt}번째 시도 : ${input}`);
-console.log(`${ball_cnt}B${strike_cnt}S`);
-
-// 완료를 했을 때
-console.log(`${try_cnt}번만의 맞히셨습니다.`);
-console.log('게임을 종료합니다.');
